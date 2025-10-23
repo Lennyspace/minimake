6e7121e57b5cefbe10964d123f04213a845d94a1
lenny.taurines <lenny.taurines@epita.fr>
2025-10-23 21:38:49 +0200

    3

--- simple_ls/simple_ls.c
+++ simple_ls/simple_ls.c
@@ -1,34 +1,34 @@
-#include <stdio.h>
 #include <dirent.h>
+#include <stdio.h>
 
+int main(int argc, char *argv[])
+{
+    char *dossier = ".";
+    if (argc == 2)
+    {
+        dossier = argv[1];
+    }
+    if (argc > 2)
+    {
+        return 1;
+    }
+    DIR *dir = opendir(dossier);
 
+    if (dir == NULL)
+    {
+        fprintf(stderr, "No such fuke ir directory\n");
+        return 1;
+    }
 
-
-
-int main(int argc,char* argv[]){
-	char* dossier=".";
-	if(argc==2){
-		dossier=argv[1];
-	}
-	if(argc>2){
-		return 1;
-	}
-	DIR* dir=opendir(dossier);
-
-
-	if(dir==NULL) {
-		fprintf(stderr,"No such fuke ir directory\n");
-		return 1;
-	}
-
-	struct dirent* file=readdir(dir);
-	while(file){
-		if(file->d_name[0]!='.'){
-			printf("%s\n",file->d_name);
-		}
-		file=readdir(dir);
-	}
-	closedir(dir);
-	return 0;
+    struct dirent *file = readdir(dir);
+    while (file)
+    {
+        if (file->d_name[0] != '.')
+        {
+            printf("%s\n", file->d_name);
+        }
+        file = readdir(dir);
+    }
+    closedir(dir);
+    return 0;
 }
-
